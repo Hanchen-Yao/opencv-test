c9fc8f9b432d170941af54fb46276cdafed0a8e3
Hanchen <64581244+Hanchen-Yao@users.noreply.github.com>
2020-05-05 14:55:22 +0800

    Update and rename show.cpp to Show.cpp

--- Show.cpp
+++ Show.cpp
@@ -3,7 +3,7 @@
 //时间：2020年5月5日
 
 #include<opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui_c.h>
+#include<opencv2/highgui/highgui_c.h>
 #include<iostream>
 
 using namespace cv;
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
 	Mat src = imread("D:/opencv_exercises-master/images/01.jpg");
 	if (src.empty()) {
 		printf("could not load image...\n");
-			return -1;
+	        return -1;
 	}
 	namedWindow("test opencv setup", CV_WINDOW_AUTOSIZE);
 	imshow("test opecv setup", src);
